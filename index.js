9e4b49bf8a1f2001e357228cfde50d1eb2bd2418
Rishabh <rdwivedi183@gmail.com>
2024-02-28 21:34:24 +0530

    setup: basic setup node project

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
